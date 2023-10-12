0e1fbd0f863605ea031bbad819401e9a05a076ab
Nathan Clack <nclack@gmail.com>
2023-10-12 11:55:20 -0700

    parser ready to test

--- sketch_oct11c/sketch_oct11c.ino
+++ sketch_oct11c/sketch_oct11c.ino
@@ -4,11 +4,173 @@ struct Camera {
     int in,out;
   } counts;
 } cameras [] =  {
-  // Using pin ids (not gpio ids)
- [0] = { .out = 1, .in = 5, .gate = 9},
- [1] = { .out = 15, .in = 19, .gate = 21},
- [2] = { .out = 34, .in = 29, .gate = 25},
+  // Using gpio ids
+ [0] = { .out = 0, .in = 3, .gate = 6},
+ [1] = { .out = 11, .in = 14, .gate = 16},
+ [2] = { .out = 19, .in = 22, .gate = 28},
 };
+
+//
+//    COMMANDS
+//
+
+
+// commands
+int emit(int camera_id) {
+  Serial.print("EMIT ");
+  Serial.println(camera_id,DEC);
+  return 0;
+}
+
+int query_out(int camera_id) {
+  Serial.print("query_out ");
+  Serial.println(camera_id,DEC);
+  return 0;
+}
+
+int query_in(int camera_id) {
+  Serial.print("query_in ");
+  Serial.println(camera_id,DEC);
+  return 0;
+}
+
+//
+//    PARSING
+//
+
+enum Result {
+  OK,
+  ERR
+};
+
+struct Command {
+  int (*cmd)(int camera_id);
+  int arg;
+};
+
+struct ParseResult {
+  enum Result result;
+  String rest;
+};
+
+// Consume digits, storing the corresponding integer in `out`.
+struct ParseResult number(int* out, struct ParseResult input) {
+  if(input.result!=OK) 
+  {
+    return input;
+  }
+
+  int cur=0,end=input.rest.length();
+  while(cur<end && isDigit(input.rest[cur])) ++cur;
+  
+  if(cur==0) {
+    // no digits
+    input.result=ERR;
+    return input;
+  } else {
+    // consume digits and translate to int
+    *out = input.rest.substring(0,cur).toInt();
+    return (struct ParseResult) {
+      .result = OK,
+      .rest = input.rest.substring(cur,end)
+    };
+  }
+}
+
+// Consume alpha characters, storing the corresponding lowercase string in `out`.
+struct ParseResult alphas(String* out, struct ParseResult input) {
+  if(input.result!=OK) 
+  {
+    return input;
+  }
+
+  int cur=0,end=input.rest.length();
+  while(cur<end && isAlpha(input.rest[cur])) ++cur;
+  
+  if(cur==0) {
+    // nothing consumed
+    input.result=ERR;
+    return input;
+  } else {
+    // consume alphas and translate to lowercase
+    *out = input.rest.substring(0,cur);
+    out->toLowerCase();
+    return (struct ParseResult) {
+      .result = OK,
+      .rest = input.rest.substring(cur,end)
+    };
+  }
+}
+
+struct ParseResult whitespace(struct ParseResult input) {
+  if(input.result!=OK) 
+  {
+    return input;
+  }
+  input.rest.trim();
+  return input;
+}
+
+
+
+// Syntax: ([a-z]+)\w(\d+), output corresponding Command in `out`
+struct ParseResult command(struct Command * out, struct  ParseResult input) {
+  static const struct {String name; int (*cmd)(int camera_id);} commands[] = {
+    {"emit", emit},
+    {"qout", query_out},
+    {"qin", query_in},
+  };
+
+  if(input.result!=OK) 
+  {
+    return input;
+  }
+
+  String cmd;
+  int camera_id=-1;
+  ParseResult result = number(&camera_id,whitespace(alphas(&cmd,input)));
+  if(result.result==OK) 
+  { 
+    for(const auto c: commands) {
+      if(cmd == c.name) {
+        *out = (struct Command) {
+          .cmd = c.cmd,
+          .arg = camera_id
+        };
+        return result;
+      }
+    }
+    // no command recognized
+    input.result==ERR;
+  }
+
+  return input;
+}
+
+struct ParseResult line(struct ParseResult *out, struct ParseResult input) {
+  out->result = ERR;
+  if(input.result!=OK) 
+  {
+    return input;
+  }
+  int i = input.rest.indexOf('\n');
+  if(i<0) {
+    input.result==ERR;
+    return input;
+  } 
+
+  *out = {
+    .result = OK,
+    .rest = input.rest.substring(0,i)
+  };
+
+  input.rest=input.rest.substring(i+1,input.rest.length());
+  return input;
+}
+
+//
+// EXECUTION
+//
 
 void setup() {
   Serial.begin(9600); 
@@ -21,70 +183,37 @@ void setup() {
   }
 }
 
-arduino::String input;
-
 // Appends any bytes from the serial port to the input stream
-void read() {
+void read(String& inbox) {
   if(Serial.available()) {
     // Get all the data available, up to the timeout
     // Avoid readStringUntil() since it also times out, but consumes the terminator.
-    input += Serial.readString();     
+    inbox += Serial.readString();     
   }
 }
 
-enum Kind {
-  EMIT,
-  QOUT,
-  QIN,
-};
-
-enum Result {
-  OK,
-  ERR
-};
-
-struct Command {
-  enum Kind kind;
-  int camera_id;
-};
-
-struct MaybeCommand {
-  enum Result result;
-  struct Command command;
-};
-
-struct MaybeCommand parse(const arduino::String& line) {
-  struct {String name; Kind kind;} commands[] = {
-    {"emit", EMIT},
-    {"qout", QOUT},
-    {"qin", QIN},
-  };
-
-  for(const auto& cmd: commands) {
-    if(String.startsWith(cmd.name))
-  }
-}
-
-void eval() {
-  // consume input dividing it into "/n" lines, until the input buffer is empty
-  {
-    int cur=0;
-    int eol=0;
-    while( (eol=input.indexOf('\n'))>=0) {
-      const auto line = input.substring(cur,eol);
-      struct comm
+void eval(String& inbox) {
+  struct ParseResult p = {.result=OK,.rest=inbox};
+  do {
+    // Consume the line, executing a command if recognized
+    struct ParseResult inner;
+    struct Command cmd;    
+    p = line(&inner,p);
+    inner = command(&cmd,inner);
+    if(inner.result==OK) {
+      // Execute 
+      cmd.cmd(cmd.arg);
     }
-  }
-
+  } while(p.result==OK);
+  inbox = p.rest;
 }
+
+
 
 void loop() {
-  // send data only when you receive data:
-  if (Serial.available() > 0) {
-    // read the incoming byte:
-    incoming = Serial.readString();
-
-    // say what you got:
-    Serial.printf("I received: %s\n\r\n",incoming.c_str());
+  arduino::String inbox;
+  while(1) {
+    read(inbox);
+    eval(inbox);
   }
 }
